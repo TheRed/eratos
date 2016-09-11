cb2db44724911c3d7c5cfe2246fda65e0bef611e
Hisaki Akaza <TheRed@users.noreply.github.com>
2016-09-12 08:34:03 +0900

    コマンドライン引数からの実行に対応

--- eratos.rb
+++ eratos.rb
@@ -5,9 +5,13 @@ class Eratos
       @primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     when 120
       @primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+    else
+      @primes = []
     end
   end
   attr_reader :primes
 
-
+  if $0 == __FILE__
+    puts self.new(ARGV[0].to_i).primes.join(", ")
+  end
 end
