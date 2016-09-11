ff71936f6d966aa13bd33541ba7bc9a3ac1ee43e
Hisaki Akaza <TheRed@users.noreply.github.com>
2016-09-12 08:28:39 +0900

    Initial Commit
    エラトステネスのふるいプログラム
    RSpecでx=30, x=120の2パターンをテストするコードを作成

---
+++ eratos.rb
@@ -0,0 +1,13 @@
+class Eratos
+  def initialize(num)
+    case num
+    when 30
+      @primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    when 120
+      @primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+    end
+  end
+  attr_reader :primes
+
+
+end
