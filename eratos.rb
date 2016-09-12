b98fcdc8297934968483615f72fa9856595705f0
Hisaki Akaza <TheRed@users.noreply.github.com>
2016-09-12 09:35:27 +0900

    デバッグ文を削除

--- eratos.rb
+++ eratos.rb
@@ -1,20 +1,12 @@
 class Eratos
   def initialize(max_num)
     search_list = [*(2..max_num)]
-    print "search_list= "
-    p search_list
-
     @primes = []
     while (search_list.first < Math.sqrt(max_num)) do
-      print "@primes= "
-      p @primes << search_list.shift
+      @primes << search_list.shift
       search_list.reject! { |num| (num % @primes.last) == 0 }
-      print "search_list_rejected= "
-      p search_list
     end
     @primes.concat(search_list)
-    print "@primes finish= "
-    p @primes
   end
   attr_reader :primes
 
